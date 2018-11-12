2d2843a325d794935c2901768fa93fb40ae6ba1c
luoqi <luoqi20131001@163.com>
2018-11-12 18:21:57 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h3>We will create a new world, great one.</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
